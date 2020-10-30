1eca624f1a48a91a572821b63142c63e8df17bb6
Henrik <henrik@teinelund.se>
2020-10-30 07:31:10 +0100

    Added module 'jproject_info-main'.

---
+++ jproject_info-main/src/test/java/com/teinelund/jproject_info/AppTest.java
@@ -0,0 +1,15 @@
+package com.teinelund.jproject_info;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    void addition() {
+        assertEquals(2, 2);
+    }
+}
