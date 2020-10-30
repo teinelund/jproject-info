735e0ec29cfb88ae2f8fb03510d0a41815ebca81
Henrik <henrik@teinelund.se>
2020-10-30 09:31:04 +0100

    Version is now fetched from pom.xml file. Added copyright text on line bellow. Added colors.

--- jproject_info-main-argument-parser/src/main/java/com/teinelund/jproject_info/main_argument_parser/Options.java
+++ jproject_info-main-argument-parser/src/main/java/com/teinelund/jproject_info/main_argument_parser/Options.java
@@ -1,10 +1,16 @@
 package com.teinelund.jproject_info.main_argument_parser;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import picocli.CommandLine;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "jproject_info", mixinStandardHelpOptions = true, version = "1.0.0-SNAPSHOT")
+@CommandLine.Command(name = "jproject_info", mixinStandardHelpOptions = true, versionProvider = com.teinelund.jproject_info.main_argument_parser.VersionProvider.class)
 public class Options implements Callable<Integer>
 {
 
@@ -12,5 +18,19 @@ public class Options implements Callable<Integer>
     public Integer call() throws Exception {
         System.out.println("Hello World Picocli.");
         return 123; // exit code
+    }
+}
+
+class VersionProvider implements CommandLine.IVersionProvider {
+    public String[] getVersion() {
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = null;
+        try {
+            model = reader.read(new FileReader("pom.xml"));
+        } catch (IOException | XmlPullParserException e) {
+            e.printStackTrace();
+            return new String[] { "Could not fetch version from application." };
+        }
+        return new String[] { "@|white Version: " +  model.getVersion() + "|@", "@|yellow Copyright (C) Teinelund 2020.|@"};
     }
 }
